71f1deddc2cb192633a61e442f0cfb1e47c693fb
Mike Lindner <mlindner9@yahoo.com>
2024-02-18 13:01:10 -0500

    Polish quiz workflow and appearance.

--- app/src/main/java/net/mikelindner/slovo/WordQuizView.kt
+++ app/src/main/java/net/mikelindner/slovo/WordQuizView.kt
@@ -1,46 +1,46 @@
 package net.mikelindner.slovo
 
-import android.media.MediaPlayer
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.ModifierLocalMap
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import net.mikelindner.slovo.ui.theme.SlovoTheme
 
@@ -59,9 +59,37 @@ fun WordQuizView(
     navBack: () -> Unit,
     bottomBar: @Composable () -> Unit
 ) {
-    var revealTranslation = remember { mutableStateOf(false) }
+    var showCheckButton = remember { mutableStateOf(true) }
+    var showNextButton = remember { mutableStateOf(false) }
+    var showTranslation = remember { mutableStateOf(false) }
     var showResult = remember { mutableStateOf(false) }
     var isCorrect = remember { mutableStateOf(false) }
+
+    val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
+
+    fun onCheckTranslation() {
+        focusManager.clearFocus()
+        isCorrect.value = vm.checkTranslation()
+        showResult.value = true
+        showTranslation.value = true
+        showCheckButton.value = false
+        showNextButton.value = true
+    }
+
+    fun onNextWord() {
+        showTranslation.value = false
+        showResult.value = false
+        showCheckButton.value = true
+        showNextButton.value = false
+        vm.nextWord()
+        focusRequester.requestFocus()
+    }
+
+    // Sets focus to focus requester initially.
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 
     Scaffold(
         bottomBar = bottomBar,
@@ -112,46 +140,51 @@ fun WordQuizView(
             Text(vm.toLanguage)
             OutlinedTextField(
                 label = { Text("Enter Translation") },
-                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
+                modifier = Modifier
+                    .focusRequester(focusRequester)
+                    .padding(top = 8.dp, bottom = 8.dp),
                 onValueChange = { vm.enteredTranslation = it },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = { onCheckTranslation() }
+                ),
                 singleLine = true,
                 value = vm.enteredTranslation
             )
 
-            Row {
-                IconButton(
-                    onClick = {
-                        isCorrect.value = vm.checkTranslation()
-                        showResult.value = true
-                        revealTranslation.value = true
+            Row() {
+                Spacer(modifier = Modifier.weight(1f))
+
+                if (showCheckButton.value) {
+                    Button(
+                        modifier = Modifier.weight(1f),
+                        onClick = { onCheckTranslation() }
+                    ) {
+                        Text(
+                            text = "Check",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp,
+                        )
                     }
-                ) {
-                    Icon(imageVector = Icons.Filled.Check, contentDescription = "Check")
                 }
 
-                IconButton(
-                    onClick = {
-                        revealTranslation.value = true
+                if (showNextButton.value) {
+                    Button(
+                        modifier = Modifier.weight(1f),
+                        onClick = { onNextWord() },
+                    ) {
+                        Text(
+                            text= "Next >",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp,
+                        )
                     }
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.question_mark_24),
-                        contentDescription = "Reveal"
-                    )
                 }
 
-                IconButton(
-                    onClick = {
-                        revealTranslation.value = false
-                        showResult.value = false
-                        vm.nextWord()
-                    }
-                ) {
-                    Icon(imageVector = Icons.Filled.KeyboardArrowRight, contentDescription = "Next")
-                }
+                Spacer(modifier = Modifier.weight(1f))
             }
 
-            if (revealTranslation.value) {
+            if (showTranslation.value) {
                 Text(
                     vm.to,
                     modifier = Modifier.padding(8.dp),
